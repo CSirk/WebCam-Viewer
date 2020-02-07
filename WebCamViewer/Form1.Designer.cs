491df7c4a9870da0d625b84fcf0302b6ba3b7b07
Cody Sirk <csirk005@gmail.com>
2020-02-07 15:57:39 -0600

    initial commit

---
+++ WebCamViewer/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace WebCamViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SourcesListBox = new System.Windows.Forms.ListBox();
+            this.ViewButton = new System.Windows.Forms.Button();
+            this.WebCamPictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.WebCamPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // SourcesListBox
+            // 
+            this.SourcesListBox.FormattingEnabled = true;
+            this.SourcesListBox.Location = new System.Drawing.Point(12, 522);
+            this.SourcesListBox.Name = "SourcesListBox";
+            this.SourcesListBox.Size = new System.Drawing.Size(120, 95);
+            this.SourcesListBox.TabIndex = 0;
+            // 
+            // ViewButton
+            // 
+            this.ViewButton.Location = new System.Drawing.Point(12, 623);
+            this.ViewButton.Name = "ViewButton";
+            this.ViewButton.Size = new System.Drawing.Size(75, 23);
+            this.ViewButton.TabIndex = 1;
+            this.ViewButton.Text = "button1";
+            this.ViewButton.UseVisualStyleBackColor = true;
+            this.ViewButton.Click += new System.EventHandler(this.ViewButton_Click);
+            // 
+            // WebCamPictureBox
+            // 
+            this.WebCamPictureBox.Location = new System.Drawing.Point(12, 0);
+            this.WebCamPictureBox.Name = "WebCamPictureBox";
+            this.WebCamPictureBox.Size = new System.Drawing.Size(1002, 506);
+            this.WebCamPictureBox.TabIndex = 2;
+            this.WebCamPictureBox.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1026, 647);
+            this.Controls.Add(this.WebCamPictureBox);
+            this.Controls.Add(this.ViewButton);
+            this.Controls.Add(this.SourcesListBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.WebCamPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox SourcesListBox;
+        private System.Windows.Forms.Button ViewButton;
+        private System.Windows.Forms.PictureBox WebCamPictureBox;
+    }
+}
+
---
+++ WebCamViewer/Form1.cs
@@ -0,0 +1,57 @@
+﻿using AForge.Video;
+using AForge.Video.DirectShow;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebCamViewer
+{
+    public partial class Form1 : Form
+    {
+        FilterInfoCollection filterInfoCollection;
+        VideoCaptureDevice videoCaptureDevice;
+
+        public Form1()
+        {
+
+            
+
+            InitializeComponent();
+
+            filterInfoCollection = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+
+            foreach (FilterInfo filterInfo in filterInfoCollection)
+            {
+                SourcesListBox.Items.Add(filterInfo.Name);
+                SourcesListBox.SelectedIndex = 0;
+                videoCaptureDevice = new VideoCaptureDevice();
+            }
+        }
+
+        private void VideoCaptureDevice_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            WebCamPictureBox.Image = (Bitmap)eventArgs.Frame.Clone();
+        }
+
+        private void ViewButton_Click(object sender, EventArgs e)
+        {
+            videoCaptureDevice = new VideoCaptureDevice(filterInfoCollection[SourcesListBox.SelectedIndex].MonikerString);
+            videoCaptureDevice.NewFrame += VideoCaptureDevice_NewFrame;
+            videoCaptureDevice.Start();
+        }
+
+        private void Form1Closing(object sender, FormClosingEventArgs e)
+        {
+            if(videoCaptureDevice.IsRunning)
+            {
+                videoCaptureDevice.Stop();
+            }
+        }
+    }
+}
